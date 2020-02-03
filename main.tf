77e2acf387e8290ffbd8c16d05425696dc2b072e
fran.gimenez <fran.gimenez@trazable.io>
2020-02-03 16:25:39 +0100

    feat(main): configure provider
    Configure google provider with the necessary values to attack the desired google project

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ terraform {
   backend "remote" {} 
 }
 
+# PROVIDER CONFIGURATION
+provider "google" {
+  credentials = file(var.google_file_account_location)
+  project     = var.google_project_id
+  region      = var.google_region
+  zone        = var.google_zone
+}
