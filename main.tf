56ed9af89261b7cb5e1a332bc397aeac6aaa0a6a
fran.gimenez <fran.gimenez@trazable.io>
2020-02-03 16:30:55 +0100

    feat(main): enable app engine
    Configure a basic app engine with hello-world to enable app engine in the target project

--- main.tf
+++ main.tf
@@ -17,3 +17,13 @@ module "initial_apis" {
 
   google_project_id = var.google_project_id
 }
+
+# DEFAULT APP ENGINE APPLICATION
+module "app_engine" {
+  source = "github.com/Trazable/Terraform-Trazable-Modules/modules/google/app-engine"
+
+  google_project_id  = var.google_project_id
+  zip_location       = var.zip_location
+  google_region      = var.google_region
+  google_location    = var.google_location
+}
